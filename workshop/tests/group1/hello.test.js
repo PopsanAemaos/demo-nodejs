f9142185f20f575c1c5b3ab404991a27d352a37d
Popsan <ford-zeros@hotmail.com>
2022-06-17 13:14:22 +0700

    run test

--- workshop/tests/group1/hello.test.js
+++ workshop/tests/group1/hello.test.js
@@ -1,9 +1,16 @@
 const request = require("supertest");
 const app = require("../../server");
 
-test('Hello World API', async () => {
+test('Test Status 200 Success', async () => {
   const response = await request(app)
       .get('/api')
     expect(response.status).toEqual(200);
     expect(response.text).toEqual('Welcome to TDD World');
 });
+
+test('Test Status 404 fail', async () => {
+  const response = await request(app)
+      .get('/api')
+    expect(response.status).toEqual(404);
+});
+
